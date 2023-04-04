cfaf2001e5a433887238a1f616aa54ab531ac5b0
Vsevolod <vsevolod.lashin@gmail.com>
2023-04-04 20:46:06 +0300

    LogInVC interface added Class WelcomeVC added

---
+++ AboutMeApp/ViewControllers/LogInViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMeApp
+//
+//  Created by Vsevolod Lashin on 04.04.2023.
+//
+
+import UIKit
+
+final class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
--- AboutMeApp/ViewControllers/WelcomeViewController.swift
+++ AboutMeApp/ViewControllers/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  AboutMeApp
 //
 //  Created by Vsevolod Lashin on 04.04.2023.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
-
