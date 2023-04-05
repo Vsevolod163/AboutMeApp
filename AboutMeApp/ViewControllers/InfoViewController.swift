c9965d88014fad89580fe2689e86c0513a09193e
Vsevolod <vsevolod.lashin@gmail.com>
2023-04-05 04:49:50 +0300

    getMyAge function added

--- AboutMeApp/ViewControllers/InfoViewController.swift
+++ AboutMeApp/ViewControllers/InfoViewController.swift
@@ -19,12 +19,25 @@ final class InfoViewController: UIViewController {
         infoLabel.text = """
                 Hello, \(user.userName ?? "")! 😊
             
-                My name is \(user.person.name) \(user.person.surname). I am 27 years old.
+                My name is \(user.person.name) \(user.person.surname). I am \(getMyAge()) years old.
                 I graduated from the Samara State University of Economics with a master's degree in world economics with honors.
                 Now I am working as a Sales Manager for a company that sells automotive paints produced by the Dutch multinational company AkzoNobel. Several of my projects were the implementation of the Akzonobel software packages for various automotive workshops.
                 I've always liked math and programming, so I decided to start studying it a year ago and I want to start working in this industry in the near future.
                 My favourite hobbies are: programming, watching movies, listening to music. Also i love to play volleyball, basketball, table tennis.
                 Looking forward to hearing from you! 😊
             """
+    }
+    
+    private func getMyAge() -> String {
+        let dateFormatter = DateFormatter()
+        var myAge = ""
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        
+        if let date = dateFormatter.date(from: user.person.dateOfBirth) {
+            let range = date..<Date.now
+            myAge = range.formatted(.components(style: .wide, fields: [.year]))
+        }
+        
+        return myAge.replacingOccurrences(of: "лет", with: "")
     }
 }
